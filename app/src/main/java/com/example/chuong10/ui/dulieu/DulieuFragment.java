a7dc1d27f3d30991f4974d9a4b5c44aba794471c
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-07-02 13:01:45 +0700

    hoanghuynh

--- app/src/main/java/com/example/chuong10/ui/dulieu/DulieuFragment.java
+++ app/src/main/java/com/example/chuong10/ui/dulieu/DulieuFragment.java
@@ -1,38 +1,49 @@
 package com.example.chuong10.ui.dulieu;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.chuong10.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.chuong10.databinding.FragmentDulieuBinding;
 
 public class DulieuFragment extends Fragment {
 
     private DulieuViewModel mViewModel;
-
-    public static DulieuFragment newInstance() {
-        return new DulieuFragment();
-    }
+    private FragmentDulieuBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dulieu, container, false);
+        binding = FragmentDulieuBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Gắn sự kiện cho nút
+        binding.btnDulieu.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), SecurityActivity.class));
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(DulieuViewModel.class);
-        // TODO: Use the ViewModel
     }
-
 }
---
+++ app/src/main/java/com/example/chuong10/ui/dulieu/SecurityUserActivity.java
@@ -0,0 +1,26 @@
+package com.example.chuong10.ui.dulieu;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chuong10.R;
+
+public class SecurityUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_security_user);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
